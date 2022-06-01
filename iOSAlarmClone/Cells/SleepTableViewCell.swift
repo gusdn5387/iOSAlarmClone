954231f79613431a78fbe6873157ca0650eb500c
박현우 <phw@baghyeon-uui-MacBookPro-2.local>
2022-06-01 22:20:10 +0900

    테이블뷰 구현

---
+++ iOSAlarmClone/Cells/SleepTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SleepTableViewCell.swift
+//  iOSAlarmClone
+//
+//  Created by 박현우 on 2022/06/01.
+//
+
+import UIKit
+
+class SleepTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- iOSAlarmClone/ViewControllers/AlarmViewController.swift
+++ iOSAlarmClone/ViewControllers/AlarmViewController.swift
@@ -8,17 +8,33 @@
 import UIKit
 
 class AlarmViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        setupLayout()
+        setupTableView()
     }
 }
 
 extension AlarmViewController {
-    // MARK: - 레이아웃 설정
-    private func setupLayout() {
+    // MARK: - 테이블뷰 설정
+    private func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "AlarmTableViewCell", bundle: nil), forCellReuseIdentifier: "alarmCell")
+    }
+}
+
+// MARK: - UITableViewDelegate, UITableViewDataSource
+extension AlarmViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "alarmCell", for: indexPath) as! AlarmTableViewCell
         
+        return cell
     }
 }

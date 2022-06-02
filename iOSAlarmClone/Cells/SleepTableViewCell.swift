4046459ee20c3e7793802f087e38bc27e78658c7
박현우 <phw@baghyeon-uui-Mac-Studio.local>
2022-06-02 11:14:31 +0900

    '수면 | 기상' 섹션 셀 구현

--- iOSAlarmClone/Cells/SleepTableViewCell.swift
+++ iOSAlarmClone/Cells/SleepTableViewCell.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class SleepTableViewCell: UITableViewCell {
-
+    @IBOutlet weak var alarmSettingBtn: UIButton!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         setupLayout()
@@ -20,5 +21,7 @@ extension SleepTableViewCell {
     // MARK: 레이아웃 설정
     private func setupLayout() {
         backgroundColor = .clear
+        alarmSettingBtn.layer.cornerRadius = alarmSettingBtn.frame.size.height / 2
+        alarmSettingBtn.layer.masksToBounds = true
     }
 }
